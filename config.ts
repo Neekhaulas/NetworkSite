200bbb22d602040aef6a0f76855281236ece4b64
Neekhaulas <nicolasroy96@gmail.com>
2019-06-27 01:09:18 +0200

    Changed endpoit

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const dev = `http://localhost:3000/`;
-export const prod = `https://35.180.116.195:3000/`;
+export const prod = `https://neekhaulas.eu:3000/`;
 
 export const endpoint = (process.env.NODE_ENV === 'development' ? dev : prod);
 export const endpointGraphQL = (process.env.NODE_ENV === 'development' ? dev : prod) + 'graphql';
