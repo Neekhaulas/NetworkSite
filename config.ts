c240e86cc956c7dbcff0eb28c74c2bbe5e645be0
Nicolas Roy <nroy@beauteprivee.com>
2019-06-26 11:11:57 +0200

    Added https

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
 export const endpoint = `http://localhost:3000/graphql`;
-export const prodEndpoint = `http://35.180.116.195:3000/graphql`;
+export const prodEndpoint = `https://35.180.116.195:3000/graphql`;
