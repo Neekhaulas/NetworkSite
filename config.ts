28636f39acd282595b0cf80495094c122d6195c7
Neekhaulas <nicolasroy96@gmail.com>
2019-06-27 23:45:09 +0200

    Changed API endpoint

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const dev = `http://localhost:3000/`;
-export const prod = `https://neekhaulas.eu:3000/`;
+export const prod = `https://api.neekhaulas.eu/`;
 
 export const endpoint = (process.env.NODE_ENV === 'development' ? dev : prod);
 export const endpointGraphQL = (process.env.NODE_ENV === 'development' ? dev : prod) + 'graphql';
